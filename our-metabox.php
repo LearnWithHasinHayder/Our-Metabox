b2e3acf98e55b255aff706aac03393f9e8a86f19
Hasin Hayder <countdraculla@gmail.com>
2018-08-20 02:32:11 +0600

    checkbox and radiobutton

--- our-metabox.php
+++ our-metabox.php
@@ -15,7 +15,7 @@ class OurMetabox {
 	public function __construct() {
 		add_action( 'plugins_loaded', array( $this, 'omb_load_textdomain' ) );
 		add_action( 'admin_menu', array( $this, 'omb_add_metabox' ) );
-		add_action( 'save_post', array( $this, 'omb_save_location' ) );
+		add_action( 'save_post', array( $this, 'omb_save_metabox' ) );
 	}
 
 	private function is_secured( $nonce_field, $action, $post_id ) {
@@ -44,40 +44,59 @@ class OurMetabox {
 
 	}
 
-	function omb_save_location( $post_id ) {
+	function omb_save_metabox( $post_id ) {
 
 		if ( ! $this->is_secured( 'omb_location_field', 'omb_location', $post_id ) ) {
 			return $post_id;
 		}
 
-		$location = isset( $_POST['omb_location'] ) ? $_POST['omb_location'] : '';
-		$country  = isset( $_POST['omb_country'] ) ? $_POST['omb_country'] : '';
+		$location    = isset( $_POST['omb_location'] ) ? $_POST['omb_location'] : '';
+		$country     = isset( $_POST['omb_country'] ) ? $_POST['omb_country'] : '';
+		$is_favorite = isset( $_POST['omb_is_favorite'] ) ? $_POST['omb_is_favorite'] : 0;
+		$colors      = isset( $_POST['omb_clr'] ) ? $_POST['omb_clr'] : array();
+		$colors2     = isset( $_POST['omb_color'] ) ? $_POST['omb_color'] : '';
 
 		if ( $location == '' || $country == '' ) {
 			return $post_id;
 		}
 
-		$location = sanitize_text_field($location);
-		$country = sanitize_text_field($country);
+		$location = sanitize_text_field( $location );
+		$country  = sanitize_text_field( $country );
 
 		update_post_meta( $post_id, 'omb_location', $location );
 		update_post_meta( $post_id, 'omb_country', $country );
+		update_post_meta( $post_id, 'omb_is_favorite', $is_favorite );
+		update_post_meta( $post_id, 'omb_clr', $colors );
+		update_post_meta( $post_id, 'omb_color', $colors2 );
 	}
 
 	function omb_add_metabox() {
 		add_meta_box(
 			'omb_post_location',
 			__( 'Location Info', 'our-metabox' ),
-			array( $this, 'omb_display_post_location' ),
-			array('post','page')
+			array( $this, 'omb_display_metabox' ),
+			array( 'post', 'page' )
 		);
 	}
 
-	function omb_display_post_location( $post ) {
-		$location = get_post_meta( $post->ID, 'omb_location', true );
-		$country  = get_post_meta( $post->ID, 'omb_country', true );
-		$label1   = __( 'Location', 'our-metabox' );
-		$label2   = __( 'Country', 'our-metabox' );
+	function omb_display_metabox( $post ) {
+		$location    = get_post_meta( $post->ID, 'omb_location', true );
+		$country     = get_post_meta( $post->ID, 'omb_country', true );
+		$is_favorite = get_post_meta( $post->ID, 'omb_is_favorite', true );
+		$checked     = $is_favorite == 1 ? 'checked' : '';
+
+		$saved_colors = get_post_meta( $post->ID, 'omb_clr', true );
+
+		$saved_color = get_post_meta( $post->ID, 'omb_color', true );
+
+		$label1 = __( 'Location', 'our-metabox' );
+		$label2 = __( 'Country', 'our-metabox' );
+		$label3 = __( 'Is Favorite', 'our-metabox' );
+		$label4 = __( 'Colors', 'our-metabox' );
+
+
+		$colors = array( 'red', 'green', 'blue', 'yellow', 'magenta', 'pink', 'black' );
+
 		wp_nonce_field( 'omb_location', 'omb_location_field' );
 		$metabox_html = <<<EOD
 <p>
@@ -87,9 +106,43 @@ class OurMetabox {
 <label for="omb_country">{$label2}: </label>
 <input type="text" name="omb_country" id="omb_country" value="{$country}"/>
 </p>
-EOD;
-		echo $metabox_html;
+<p>
+<label for="omb_is_favorite">{$label3}: </label>
+<input type="checkbox" name="omb_is_favorite" id="omb_is_favorite" value="1" {$checked} />
+</p>
 
+<p>
+<label>{$label4}: </label>
+
+EOD;
+
+		foreach ( $colors as $color ) {
+			$_color       = ucwords( $color );
+			$checked      = in_array( $color, $saved_colors ) ? 'checked' : '';
+			$metabox_html .= <<<EOD
+<label for="omb_clr_{$color}">{$_color}</label>
+<input type="checkbox" name="omb_clr[]" id="omb_clr_{$color}" value="{$color}" {$checked}  />
+EOD;
+		}
+
+		$metabox_html .= "</p>";
+
+		$metabox_html .= <<<EOD
+<p>
+<label>{$label4}: </label>
+EOD;
+
+		foreach ( $colors as $color ) {
+			$_color       = ucwords( $color );
+			$checked      = ( $color == $saved_color ) ? "checked='checked'" : '';
+			$metabox_html .= <<<EOD
+<label for="omb_color_{$color}">{$_color}</label>
+<input type="radio" name="omb_color" id="omb_color_{$color}" value="{$color}" {$checked}  />
+EOD;
+		}
+
+		$metabox_html .= "</p>";
+		echo $metabox_html;
 	}
 
 
